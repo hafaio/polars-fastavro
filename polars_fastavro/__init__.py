08b33a4b100f37cbdc9a8544e7a2a501d1b77336
Erik Brinkman <erik.brinkman@gmail.com>
2025-04-11 00:23:12 -0400

    first version
    Summary:
    
    Test Plan:

--- polars_fastavro/__init__.py
+++ polars_fastavro/__init__.py
@@ -1,20 +1,20 @@
-# polars-fastavro
-
-A polars io-plugin that wraps fastavro
+r"""Polars io-plugin backed by fastavro.
 
 This plugin allows reading, writing, and scanning avro files into polars
 DataFrames using the fastavro library.
 
-## Usage
+Usage
+-----
 
-```py
-from polars_fastavro import scan_avro, read_avro, write_avro
+ .. code-block:: python
 
-frame = scan_avro(...).collect()  # read_avro()
-write_avro(frame, dest)
-```
+    from polars_fastavro import scan_avro, read_avro, write_avro
 
-## Limitations
+    frame = scan_avro(...).collect()  # or `read_avro(...)`
+    write_avro(frame, dest)
+
+Limitations
+-----------
 
 1. Because it uses python types an an intermediary, it's slow, (30x read to 80x
    write).
@@ -24,8 +24,14 @@ write_avro(frame, dest)
 4. Additionally, some types could in theory be supported by aren't for technical
    reasons. These include fixed, decimal, uuid, time, and
    duration.
-5. Timestamp support is limited. local-timestamp-*s are treated as Datetime
-   without tz info, while timestamp-*s are reated as UTC Datetime. Writing
+5. Timestamp support is limited. local-timestamp-\*s are treated as Datetime
+   without tz info, while timestamp-\*s are reated as UTC Datetime. Writing
    Datetimes with nano-precision is also not supported.
 6. This can't read cloud files, as that functionality isn't exposed in python to
    my knowledge.
+"""
+
+from ._scan import read_avro, scan_avro
+from ._sink import write_avro
+
+__all__ = ("scan_avro", "read_avro", "write_avro")
