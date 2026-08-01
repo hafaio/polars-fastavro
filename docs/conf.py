229ee6b0d577ae48ffdc50f02b3f7092acab0274
Erik Brinkman <erik.brinkman@gmail.com>
2026-08-01 00:48:45 -0400

    chore: remove tomllib (#31)

--- docs/conf.py
+++ docs/conf.py
@@ -2,15 +2,11 @@
 
 import sys
 from datetime import date
+from importlib.metadata import version as package_version
 from os import path
-
-import tomllib
 
 # make sure one level up is on python path
 sys.path.append(path.abspath(".."))
-
-with open("../pyproject.toml", "rb") as fh:
-    toml = tomllib.load(fh)
 
 extensions = [
     "sphinx.ext.coverage",
@@ -19,11 +15,9 @@ extensions = [
     "myst_parser",
 ]
 
-pyproject = toml["project"]
-
-project = pyproject["name"]
-version = pyproject["version"]
-release = pyproject["version"]
+project = "polars-fastavro"
+version = package_version(project)
+release = version
 
 copyright = f"{date.today().year:d} Erik Brinkman"  # noqa: A001
 author = "Erik Brinkman"
